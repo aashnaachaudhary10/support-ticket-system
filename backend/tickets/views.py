b1a6d9d94149c7d2c40327d07ac830dc41470bd7
AASHNA CHAUDHARY <jchaudharyy10@gmail.com>
2026-02-18 15:27:28 +0530

    Implement ticket viewset with filtering and search

--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .models import Ticket
+from .serializers import TicketSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+
+    filterset_fields = ["category", "priority", "status"]
+    search_fields = ["title", "description"]
+    ordering = ["-created_at"]
